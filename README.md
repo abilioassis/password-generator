82c54691daa68928be8456fb801f6fcffeec85d0
Abilio de Assis <abilio.assis@petrobras.com.br>
2023-04-10 17:11:59 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,23 @@
+# Scrimba Bootcamp - Exclusive Solo Projects
+
+This is a solution to the [M3 - Password Generator](https://scrimba.com/learn/frontend/solo-project-pro-password-generator-cR9B46Sg).
+
+## Screenshot
+
+![](img/screenshot.png)
+
+## Links
+
+- Live Site: [Netlify]()
+- Codebase: [Github](https://github.com/abilioassis/password-generator)
+- Code review: [Scrimba]()
+
+## Built with
+
+- Semantic HTML5 markup.
+- Flexbox.
+- No frameworks (only pure HTML and CSS 🏅).
+
+## Author
+
+- [Abilio Assis](https://www.linkedin.com/in/abilio-assis/)
---
+++ index.js
@@ -0,0 +1,129 @@
+"use strict";
+
+/**
+ * Characters that make up the password
+ */
+const characters = [
+  "A",
+  "B",
+  "C",
+  "D",
+  "E",
+  "F",
+  "G",
+  "H",
+  "I",
+  "J",
+  "K",
+  "L",
+  "M",
+  "N",
+  "O",
+  "P",
+  "Q",
+  "R",
+  "S",
+  "T",
+  "U",
+  "V",
+  "W",
+  "X",
+  "Y",
+  "Z",
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z",
+  "0",
+  "1",
+  "2",
+  "3",
+  "4",
+  "5",
+  "6",
+  "7",
+  "8",
+  "9",
+  "~",
+  "`",
+  "!",
+  "@",
+  "#",
+  "$",
+  "%",
+  "^",
+  "&",
+  "*",
+  "(",
+  ")",
+  "_",
+  "-",
+  "+",
+  "=",
+  "{",
+  "[",
+  "}",
+  "]",
+  ",",
+  "|",
+  ":",
+  ";",
+  "<",
+  ">",
+  ".",
+  "?",
+  "/",
+];
+
+/**
+ * Loads random passwords into screen fields
+ * Generates a random password of passwordLenght characters
+ */
+function getPassword() {
+  const pwd1El = document.getElementById("pwd1"),
+    pwd2El = document.getElementById("pwd2");
+  pwd1El.textContent = generatePassword();
+  pwd2El.textContent = generatePassword();
+}
+
+/**
+ * Generates a random password of "passwordLenght" characters
+ */
+function generatePassword() {
+  const passwordLength = 15;
+  let pwd = "";
+  for (let i = 0; i < passwordLength; i++) {
+    pwd += getRandomChar();
+  }
+  return pwd;
+}
+
+/**
+ * Returns a random character drawn from the array characters
+ */
+function getRandomChar() {
+  let idx = Math.floor(Math.random() * characters.length);
+  return characters[idx];
+}
